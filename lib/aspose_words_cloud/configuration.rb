0e215cebb8d9e7dd68cb685981388864566da295
avinash-s <avinash.s@freshworks.com>
2025-01-28 17:21:56 +0530

    [FS-206909] revert logger change

--- lib/aspose_words_cloud/configuration.rb
+++ lib/aspose_words_cloud/configuration.rb
@@ -98,7 +98,7 @@ module AsposeWordsCloud
       @client_data_prefix = {}
       @client_side_validation = true
       @debugging = false
-      @logger = Rails.logger || Logger.new($stdout)
+      @logger = defined?(Rails) ? Rails.logger : Logger.new(STDOUT)
 
       yield(self) if block_given?
     end
